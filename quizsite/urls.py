0e593799fb2c111ed21e4ed70e61990f58a215ee
SusLin77 <lindeborgsusanne@gmail.com>
2017-09-27 16:16:32 +0200

    Named all url:s and made path for .html

--- quizsite/urls.py
+++ quizsite/urls.py
@@ -3,7 +3,7 @@ from quiz import views
 
 urlpatterns = [
 	path("", views.startpage, name="start_page"),
-	path("quiz/<int:quiz_number>/", views.quiz name="quiz_page"),
-	path("quiz/<int:quiz_number>/question/<int:question_number>/", views.question name="question_page"),
-	path("quiz/<int:quiz_number>/completed/", views.completed name="completed_page"),
+	path("quiz/<int:quiz_number>/", views.quiz, name="quiz_page"),
+	path("quiz/<int:quiz_number>/question/<int:question_number>/", views.question, name="question_page"),
+	path("quiz/<int:quiz_number>/completed/", views.completed, name="completed_page"),
 ]
